043f62fedc070233b5c0c8e302e1a833ef247953
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-03-04 17:02:35 -0700

    update artillery.yml file, and db connection

--- server/db/config.js
+++ server/db/config.js
@@ -1,20 +1,20 @@
 const Pool = require('pg').Pool;
-// connection to the Postgres DB
-const pool = new Pool({
-  user: 'robgonzalez-pita',
-  host: 'localhost',
-  database: 'productoverview',
-  password: 'password',
-  port: 5432,
-});
-
-//ec2 instance connection
+// connection to the local Postgres DB
 // const pool = new Pool({
-//   user: 'postgres',
-//   host: '3.16.10.167',
+//   user: 'robgonzalez-pita',
+//   host: 'localhost',
 //   database: 'productoverview',
-//   password: 'hello',
+//   password: 'password',
 //   port: 5432,
 // });
+
+// ec2 instance connection
+const pool = new Pool({
+  user: 'postgres',
+  host: '3.16.10.167',
+  database: 'productoverview',
+  password: 'hello',
+  port: 5432,
+});
 
 module.exports = pool;
